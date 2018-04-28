1775c5aa62a73a66f0fa74a98cb12b5e024049d8
aswinramani <aswinramani5@gmail.com>
2018-04-28 10:35:36 +0530

    add image clf project and style to details

--- src/components/details.vue
+++ src/components/details.vue
@@ -1,20 +1,23 @@
 <template>
   <b-container class="details">
-    <b-row>
-      <b-col class="text-left">
-        <router-link :to="{ name: 'projects'}" >  
-          <b-button size="sm" variant="primary">Back</b-button>
-        </router-link> 
-      </b-col>
-    </b-row>  
     <b-row class="justify-content-md-center">
       <b-col md>
+      <b-jumbotron text-variant="black" border-variant="light">
+        <template slot="header">
+          {{project.title}}
+        </template>
         <article>
-          <h5>{{$route.params.name}}</h5>
+          <div class="heading">Background information</div>
+          <div class="heading">Solution</div>
+          <div class="heading">Software and Libraries</div>
         </article>
-      </b-col> 
+        <router-link :to="{ name: 'projects'}" >  
+            <b-button size="md" variant="primary">Back</b-button>
+        </router-link>
+      </b-jumbotron>
+      </b-col>
     </b-row>
-  </b-container>    
+  </b-container>
 </template>
 <script>
 import projects  from '@/components/projects'
@@ -24,12 +27,23 @@ export default {
     projects
   },
   data() {
+    var _parent = projects.data()
+    var name = this.$route.params.name
+    var project = _parent.projects.filter(function(o){ return o.name == name})
     return {
-      msg: "Details"
+      msg: "Details",
+      project: project[0]
     }
   }
 }
 </script>
 <style scoped>
+.display-3{
+  font-size: 3.5rem;
+}
+.heading {
+  font-weight: 300;
+  font-size: 1.2rem;
+}
 
 </style>
--- src/components/projects.vue
+++ src/components/projects.vue
@@ -1,7 +1,7 @@
 <template>
   <b-container class="projects">
       <b-row class="justify-content-md-center" v-for="project in projects" :key="project.id">
-        <b-col col md>
+        <b-col col md="8" id="project-wrapper">
           <article id="project-content">
             <h4>{{project.title}}</h4>
             <p>{{project.overview}}</p>
@@ -21,7 +21,8 @@ export default {
       msg: "Projects",
       projects: [
         {id: 1, name: "object-detection", title: "Object Detection", overview: "Developed a algorithm to detect objects within images captured by drone. "},
-        {id: 2, name: "stock-price-prediction", title: "Stock Price Predictor", overview: "Developed a stock price predictor that makes use of past trading data to produce projected estimates. The dataset that would be used to develop the machine learning model will predict the adjusted close price of a stock, which is a numerical value. The prediction would be made for the next seven trading days."}
+        {id: 2, name: "stock-price-prediction", title: "Stock Price Predictor", overview: "Developed a stock price predictor that makes use of past trading data to produce projected estimates. The dataset that would be used to develop the machine learning model will predict the adjusted close price of a stock, which is a numerical value. The prediction would be made for the next seven trading days."},
+        {id: 3, name: "image-classification", title: "Image Classification", overview: "Developed a Convolutional Neural Network based dog breed classifier that accepts images from the user as input and predicts the breed."},
       ]
     }
   }
@@ -29,10 +30,12 @@ export default {
 </script>
 <style scoped>
 .projects {
-  background-color: #f7f7f7;
   padding-top: 20px;
   padding-bottom: 20px;
-  border-radius: 10px;
+}
+#project-wrapper {
+  padding: 15px;
+  background-color: #f7f7f7;
 }
 #project-content {
   padding: 20px 0px 20px 0px;
